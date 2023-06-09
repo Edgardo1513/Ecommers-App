a221b009b64d4a421a85c0f3164a26faa0913279
Edgardo Rafael Oñate Povea <edgardorfael0472@gmail.com>
2023-06-08 19:42:59 -0500

    version-2

--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -1,10 +1,53 @@
-import React from 'react';
+import Button from 'react-bootstrap/Button';
+import Col from 'react-bootstrap/Col';
+import Form from 'react-bootstrap/Form';
+import Row from 'react-bootstrap/Row';
+import { useForm } from 'react-hook-form';
 
 
 const Login = () => {
+
+    const { register, handleSubmit } = useForm()
+
+    const submit = data => {
+        console.log(data)
+    }
     return (
         <div>
-            <h1>Login</h1>
+            <Form onSubmit={ handleSubmit(submit) } className='p-3' style={{border:"1px solid black"}}>
+                <h1>Inicia sesión</h1>
+                <Form.Group as={Row} className="mb-3" controlId="formHorizontalEmail">
+                    <Form.Label column sm={2}>
+                    Email
+                    </Form.Label>
+                    <Col sm={10}>
+                        <Form.Control 
+                        type="email"
+                        placeholder="Email" 
+                        { ...register("email") }
+                        />
+                    </Col>
+                </Form.Group>
+
+                <Form.Group as={Row} className="mb-3" controlId="formHorizontalPassword">
+                    <Form.Label column sm={2}>
+                    Password
+                    </Form.Label>
+                    <Col sm={10}>
+                        <Form.Control 
+                        type="password" 
+                        placeholder="Password" 
+                        { ...register("password") }
+                        />
+                    </Col>
+                </Form.Group>          
+                <Form.Group as={Row} className="mb-3">
+                    <Col sm={{ span: 10, offset: 2 }}>
+                        <Button type="submit">Sign in</Button>
+                    </Col>
+                 </Form.Group>
+
+            </Form>
         </div>
     );
 };
--- src/pages/ProductsDetail.jsx
+++ src/pages/ProductsDetail.jsx
@@ -2,19 +2,30 @@ import axios from 'axios'
 import { useState, useEffect } from 'react';
 import { useParams } from 'react-router-dom';
 import Button from 'react-bootstrap/esm/Button';
-
+import Row from 'react-bootstrap/Row';
+import Col from 'react-bootstrap/Col';
+import { useDispatch, useSelector} from 'react-redux';
+import { filterCategoryThunk } from '../store/slices/products.slice';
+import ListGroup from 'react-bootstrap/ListGroup';
 
 const ProductsDetail = () => {
-const { id } = useParams()
-const [ products, setproducts ] = useState({})
-const [ rate, setRate ] = useState(1)
+
+    const { id } = useParams()
+    const [ products, setproducts ] = useState({})
+    const [ rate, setRate ] = useState(1)
+    const dispatch = useDispatch()
+    const allProducts = useSelector(state => state.products)
+    const prouctsFiltered = allProducts.filter( products => products.id != parseInt(id))
 
 useEffect (() => {
+    
     axios
     .get(`https://e-commerce-api-v2.academlo.tech/api/v1/products/${id}`)
     .then( resp => {        
         setproducts(resp.data)
+        dispatch(filterCategoryThunk(resp.data.category.id))
     })
+
 }, [])
 
     const decrements = () => {
@@ -22,8 +33,9 @@ useEffect (() => {
             setRate( rate - 1 )
         }
     }
+
     return (
-        <div>
+        <div className='py-5'>
              <h1>{ products.title } </h1>
             <p>By: {products.brand} </p>            
             <p>{products.createdAt} </p>
@@ -34,7 +46,26 @@ useEffect (() => {
                 <span>{rate}</span>
                 <Button onClick={ () => setRate(rate + 1)}>+</Button>
 
-                <Button className='primary mx-3'>Agregar a Favorite</Button>
+                <Button className='primary ms-3'>Agregar a Favorite</Button>
+                <Row className='pt-3'>
+                    <Col lg={9} >
+                        { /*Products */} 
+                        <img src={products.images?.[0].url} alt="" className='img-fluid mt-3 mb-3 mx-3'  />
+                        <small className='mb-3 d-block'>{products.description} </small>
+                        
+                    </Col>
+                    <Col lg={3} >                     
+                    <h3>Related Products </h3>
+                     <ListGroup>
+                        {
+                         prouctsFiltered.map( products => (
+                            <ListGroup.Item key={products.id} >{products.title} </ListGroup.Item>                      
+
+                         ))
+                        }
+                    </ListGroup>
+                    </Col>
+                </Row>
             </div>
         </div>
     );
@@ -42,4 +73,4 @@ useEffect (() => {
 
 
 
-export default ProductsDetail;
+export default  ProductsDetail;
