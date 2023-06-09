842a7833481c552eba746ecd9229d79a470f8ee4
Edgardo Rafael Oñate Povea <edgardorfael0472@gmail.com>
2023-06-09 07:02:12 -0500

    version-3

--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -3,14 +3,36 @@ import Col from 'react-bootstrap/Col';
 import Form from 'react-bootstrap/Form';
 import Row from 'react-bootstrap/Row';
 import { useForm } from 'react-hook-form';
+import axios from 'axios';
+import { setIsLoading } from '../store/slices/isLoading.slice';
+import { useDispatch } from 'react-redux';
+import { useNavigate } from 'react-router-dom';
+
 
 
 const Login = () => {
 
     const { register, handleSubmit } = useForm()
+    const dispatch = useDispatch()
+    const navigate = useNavigate()
 
     const submit = data => {
-        console.log(data)
+        dispatch( setIsLoading(true))
+
+        axios
+        .post("https://e-commerce-api-v2.academlo.tech/api/v1/users/login", data)
+        .then( resp => {
+            console.log(resp)
+            localStorage.setItem("token", resp.data.access)
+            navigate("/")
+        })
+        .catch(error => {
+            if(error.response.status === 401){
+                alert("Credenciales incorrectas")
+            }
+            console.error(error)
+        } )
+        .finally( () => dispatch( setIsLoading(false) ) )
     }
     return (
         <div>
